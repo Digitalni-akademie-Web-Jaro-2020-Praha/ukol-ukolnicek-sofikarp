c3d99c2c044d4e699bea7650e44c17f2984e0c8a
Sofie Karpowiczova <sofikarp@gmail.com>
2020-05-16 14:52:15 +0200

    Vypracování úkolu.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('funguju!');
+const toDoTasksElm = document.querySelector(".todo__tasks")
+
+const tasks = []
+
+const updateTasks = () => {
+  toDoTasksElm.innerHTML = ""
+  for (let i = 0; i < tasks.length; i += 1) {
+    toDoTasksElm.innerHTML += `<div class="task">${tasks[i]}</div>`
+  }
+};
+
+const buttonElm = document.querySelector(".btn-add");
+buttonElm.addEventListener("click", () => {
+  const inputElm = document.querySelector("#new-task")
+  tasks.push(inputElm.value);
+  inputElm.value = "";
+  updateTasks();
+})
+
